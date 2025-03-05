c021cc2a5969446bed83c49b9935a87d76211ffd
Burhanverse <burhanverse@gmail.com>
2025-03-05 22:03:29 +0530

    update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt install wget
-wget -qO- https://raw.githubusercontent.com/burhancodes/deb/main/fagram_repo.asc | sudo tee /etc/apt/trusted.gpg.d/fagram_repo.asc
-echo "deb [arch=amd64] https://raw.githubusercontent.com/burhancodes/deb/main/apt/repo/ bionic main" | sudo tee /etc/apt/sources.list.d/fagram.list
+wget -qO- https://raw.githubusercontent.com/burhancodes/fagram-deb/main/fagram_repo.asc | sudo tee /etc/apt/trusted.gpg.d/fagram_repo.asc
+echo "deb [arch=amd64] https://raw.githubusercontent.com/burhancodes/fagram-deb/main/apt/repo/ bionic main" | sudo tee /etc/apt/sources.list.d/fagram.list
 sudo apt update && sudo apt install fagram -y
